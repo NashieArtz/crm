4d8b0afcf29deddbdfc507cab45f6012ff1c25e3
Florent Zysk <zysk.florent@nfs.school>
2026-03-27 16:02:47 +0000

    Fix/back dashboard

--- app/Http/Controllers/OpportunityController.php
+++ app/Http/Controllers/OpportunityController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Opportunity;
+use App\Http\Requests\OpportunityRequest;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class OpportunityController extends Controller
 {
-    //
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
@@ -10,7 +10,7 @@ Route::inertia('/', 'welcome', [
 ])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'dashboard')->name('dashboard');
+    Route::get('dashboard', DashboardController::class)->name('dashboard');
 });
 
 Route::middleware(['auth'])->group(function () {
@@ -18,4 +18,3 @@ Route::middleware(['auth'])->group(function () {
 });
 
 require __DIR__.'/settings.php';
-
