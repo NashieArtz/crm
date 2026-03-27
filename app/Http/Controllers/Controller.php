e488a08af06e6d68255531682a182a27d37b88c5
WU Ange <wu.ange@nfs.school>
2026-03-27 10:20:04 +0100

    fix: Accumulative income per month

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    public function formatPrice(int $price): string
+    public function formatPrice($price): string
     {
         return number_format($price, 2, '.', ',').'€';
     }
--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Activity;
 use App\Models\Client;
 use App\Models\Opportunity;
+use DB;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -39,14 +40,95 @@ class DashboardController extends Controller
         $opportunityTotalIncome = Opportunity::whereIn('status', ['closed_won'])->sum('amount');
         $opportunityPotentialTotalIncome = $opportunityPotentialIncome + $opportunityTotalIncome;
 
+        // <editor-fold desc="TOTAL ACCUMULATED INCOME PER MONTH">
+        $monthlyIncome = [];
+        $totalIncome = 0;
 
-        $incomePerWeek = Opportunity::whereIn('status', ['closed_won'])
+        // Requête
+        $incomeDataMonth = Opportunity::where('status', 'closed_won')
+            ->where('updated_at', '>=', now()->subYear())
+            ->orderBy('updated_at', 'asc')
+            ->get();
+
+        // Boucle sur chaque opportunités
+        foreach ($incomeDataMonth as $month) {
+            $monthFormat = $month->updated_at->format('M Y');
+
+            // If month !exists in array, initialize
+            if (! isset($monthlyIncome[$monthFormat])) {
+                $monthlyIncome[$monthFormat] = [
+                    'month' => $monthFormat,
+                    'amount' => 0,
+                    'total' => 0,
+                ];
+            }
+
+            // Montant du mois en cours
+            $monthlyIncome[$monthFormat]['amount'] += (float) $month->amount;
+
+        }
+        // Cumulation du montant
+        foreach ($monthlyIncome as $data) {
+            $totalIncome += $data['amount'];
+            $data['total'] += $totalIncome;
+        }
+        // </editor-fold>
+
+        // <editor-fold desc="INCOME PER WEEK OVER 6 MONTHS">
+        $weeklyIncome = [];
+
+        $incomeDataWeek = Opportunity::where('status', 'closed_won')
+            ->where('updated_at', '>=', now()->subMonths(6))
+            ->get();
+
+        foreach ($incomeDataWeek as $data) {
+            $weekFormat = 'Sem. '.$data->updated_at->format('W');
+
+            if (! isset($weeklyReport[$weekFormat])) {
+                $weeklyReport[$weekFormat] = [
+                    'label' => $weekFormat,
+                    'income' => 0,
+                ];
+            }
+
+            $weeklyIncome[$weekFormat]['income'] += (float) $data->amount;
+        }
+        // </editor-fold>
+
+        // <editor-fold desc="INCOME PER DAY PER WEEK">
+        $incomeDataWeek = Opportunity::where('status', ['closed_won'])
             ->where('updated_at', '>=', now()->subWeek())
-            ->sum('amount');
-        
-        $incomePerMonth = Opportunity::whereIn('status', ['closed_won'])
-            ->where('updated_at', '>=', now()->subMonth())
-            ->sum('amount');
+            ->select([
+                DB::raw('DATE(updated_at) as date'),
+                DB::raw('SUM(amount) as total'),
+            ])
+            ->groupBy('date')
+            ->orderBy('date', 'asc')
+            ->get();
+        $incomePerDayPerWeek = $incomeDataWeek->map(fn ($item) => [
+            'day' => date('d/m', strtotime($item->date)),
+            'amount' => (float) $item->total,
+        ]);
+        // </editor-fold>
+
+        // <editor-fold desc="INCOME PER DAY PER MONTH">
+        $incomeDataMonth = Opportunity::where('status', 'closed_won')
+            ->where('updated_at', '>=', now()->subDays(30))
+            ->select([
+                // DATE() keeps the date only
+                DB::raw('DATE(updated_at) as date'),
+                DB::raw('SUM(amount) as total'),
+            ])
+            // Regroupage des montants par date
+            ->groupBy('date')
+            ->orderBy('date', 'asc')
+            ->get();
+        $incomePerDayPerMonth = $incomeDataMonth->map(fn ($item) => [
+            // Format day-Month
+            'day' => date('d-M', strtotime($item->date)),
+            'amount' => (float) $item->total,
+        ]);
+        // </editor-fold>
 
         // Dernières opportunités
         $latestOpportunities = Opportunity::with('orders')->latest()->limit(10)->get();
@@ -72,18 +154,21 @@ class DashboardController extends Controller
                 $query->latest();
             }])->latest()->limit(10)->get();
 
-
         // Dernières activités
         $latestActivities = Activity::latest()->limit(10)->get();
-
 
         // Envoi des données vers dashboard
         return Inertia::render('dashboard', [
             'totalClient' => $totalClient,
             'totalOpportunities' => $totalOpportunities,
 
-            'incomePerWeek' => $incomePerWeek,
-            'incomePerMonth' => $incomePerMonth,
+            'incomePerDayPerWeek' => $incomePerDayPerWeek,
+            'incomePerDayPerMonth' => $incomePerDayPerMonth,
+
+            'incomeData' => [
+                'monthlyIncome' => array_values($monthlyIncome),
+                'weeklyIncome' => array_values($weeklyIncome),
+            ],
 
             'opportunityPotentialIncome' => $opportunityPotentialIncome,
             'opportunityTotalIncome' => $opportunityTotalIncome,
